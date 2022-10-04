9c18f0ed25f84522eb91b28bd3256e2f6e78672d
Alan Garin <garinsabino@gmail.com>
2022-10-04 17:29:44 -0300

    Finalizei o cronometro

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ let horas = 00
 let minutos = 00
 let segundos = 00
 
+let intervalo 
+
+
+function doisDig (digito) {
+    if(digito <10) {
+        return('0' + digito)
+    } else {
+        return(digito)
+    }
+}
+
 function começar() {
-    setInterval(watch,1000)
+    watch()
+   intervalo = setInterval(watch,1000)
 }
 
 function parar() {
-    console.log('Parou!')
+   clearInterval (intervalo)
 } 
 
 function resetar() {
-    console.log('Resetou!')
+    clearInterval (intervalo)
+    segundos = 0 
+    minutos = 0
+    document.getElementById('watch').innerHTML= '00:00:00'
 }
 
 // Relógio
@@ -20,6 +35,10 @@ function watch(){
     if(segundos == 60) {
         minutos++
         segundos=0
+        if(minutos==60) {
+            minutos=0
+            horas++
+        }
     }
-    document.getElementById('watch').innerHTML= minutos +':'+ segundos
+    document.getElementById('watch').innerHTML= doisDig(horas) +':'+ doisDig(minutos) +':'+ doisDig(segundos)
 }
